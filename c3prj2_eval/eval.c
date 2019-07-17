46164f135f827b39fb74ee9f2e7fb3443b0913e6
Coursera Learner <nobody@nowhere.nul>
2019-07-17 05:21:50 +0000

    Revised eval.c

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -7,7 +7,7 @@ int card_ptr_comp(const void * vp1, const void * vp2) {
   const card_t * const * cp1 = vp1;
   const card_t * const * cp2 = vp2;
 
-  return ((*cp2)->value == (*cp1)->value) ? ((*cp1)->suit - (*cp2)->suit) : ((*cp2)->value - (*cp1)->value);
+  return ((*cp2)->value == (*cp1)->value) ? ((*cp2)->suit - (*cp1)->suit) : ((*cp2)->value - (*cp1)->value);
 }
 
 suit_t flush_suit(deck_t * hand) {
