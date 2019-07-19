f2d8b34607f4015c585738eb4c55c9c10e8adfd8
Coursera Learner <nobody@nowhere.nul>
2019-07-19 00:04:45 +0000

    Did assignment 30

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -19,6 +19,79 @@ void sortData(char ** data, size_t count) {
 int main(int argc, char ** argv) {
   
   //WRITE YOUR CODE HERE!
+  // char ** lines = NULL;
+  //char * curr = NULL;
+  //size_t sz = 0;
+  //size_t row = 0;
+  //FILE * source = NULL;
+  //ssize_t len = 0;
+  // No argument input: read from stdin
+  if (1 == argc) {
+    // printf("No commandline argument: read input from keyboard:\n");
+
+    //source = stdin;
+     char ** lines = NULL;
+     char * curr = NULL;
+     size_t sz = 0;
+     size_t row = 0;
+
+    while(getline(&curr, &sz, stdin) >= 0) {
+      // read line
+      lines = realloc(lines, (row + 1) * sizeof(*lines));
+      lines[row] = curr;
+      curr = NULL;
+      row++;
+    }
+    free(curr);
+    // sort line
+    sortData(lines, row);
+    // print line
+    for (size_t i = 0; i < row; i++) {
+      printf("%s", lines[i]);
+      free(lines[i]);
+    }
+    free(lines);
+    
+  }
+
+  // free(lines);
+
+
+  if (1 < argc) {
+
+    for (size_t i = 0; i < argc - 1; i++) {
+      // printf("Read from file: %s\n",argv[i+1]);
+
+       char ** lines = NULL;
+       char * curr = NULL;
+       size_t sz = 0;
+       size_t row = 0;
   
+      FILE * f = fopen(argv[i+1], "r");
+      if (NULL == f) {
+	perror("Cannot open file!\n");
+	return EXIT_FAILURE;
+      }
+      //  source = f;
+
+      while(getline(&curr, &sz, f) >= 0) {
+      // read line
+	lines = realloc(lines, (row + 1) * sizeof(*lines));
+	lines[row] = curr;
+	curr = NULL;
+	row++;
+      }
+      free(curr);
+      // sort line
+      sortData(lines, row);
+      // print line
+      for (size_t i = 0; i < row; i++) {
+	printf("%s", lines[i]);
+	free(lines[i]);
+      }
+      free(lines);
+      fclose(f);
+  }
+  }
   return EXIT_SUCCESS;
 }
